56e5f91a4ef492f49d5be1b7545051e1f2760cff
yudai-nishimura <yudai.n0628@gmail.com>
2023-03-11 18:24:58 +0900

    Add: spec、gemを追加

--- spec_back_up/factories/users.rb
+++ spec_back_up/factories/users.rb
@@ -6,9 +6,5 @@ FactoryBot.define do
     name { Faker::Name.name }
     password { 'password' }
     password_confirmation { 'password' }
-
-    trait :woman_user do
-      gender { :woman }
-    end
   end
 end
